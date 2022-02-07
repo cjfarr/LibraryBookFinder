517f8170dde5aff559c1e09a07dba836454f502e
CJ Farr <chrisjfarr@outlook.com>
2022-02-07 17:18:49 -0600

    Finished up the clock art

--- LibraryBookFinder/Views/MainView.xaml.cs
+++ LibraryBookFinder/Views/MainView.xaml.cs
@@ -28,6 +28,9 @@
             Rectangle minuteHand = canvasChildren.FirstOrDefault(c => c.Name == "clockMinuteHand") as Rectangle;
             this.clockArtMinuteHand = minuteHand.RenderTransform as RotateTransform;
 
+            Rectangle hourHand = canvasChildren.FirstOrDefault(c => c.Name == "clockHourHand") as Rectangle;
+            this.clockArtHourHand = hourHand.RenderTransform as RotateTransform;
+
             this.OnTimerTick(this, null);
         }
 
@@ -37,16 +40,21 @@
             set;
         }
 
-        protected override TimeSpan GetTimeInterval()
-        {
-            return new TimeSpan(0, 0, 1);
-        }
-
         protected override void OnTimerTick(object sender, EventArgs e)
         {
             double minutePercentage = DateTime.Now.Second / 60d;
             this.clockArtMinuteHand.Angle = Math.Floor(360d * minutePercentage);
 
+            int hour = DateTime.Now.Hour;
+            if (hour > 12)
+            {
+                hour -= 12;
+            }
+
+            double hourPercentage = hour / 12d;
+            ////the  - .25 is because I originally drew the hour hand on 3 instead of 12.
+            this.clockArtHourHand.Angle = Math.Floor(360d * (hourPercentage - .25d));
+            
             this.currentDateTextBlock.Text = DateTime.Now.ToString("dddd, MM/dd");
             this.currentTimeTextBlock.Text = DateTime.Now.ToString("h:mm tt");
         }
